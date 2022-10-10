26604901ff2b8e2a604b098d9b346cc5abdf9aaf
Alex <alex_step04@mail.ru>
2022-10-10 22:22:29 +0300

    npm package relese

--- frame.js
+++
@@ -1,9 +0,0 @@
-class Frame {
-    content;
-    classList;
-
-    constructor(content, classList) {
-        this.content = content;
-        this.classList = classList;
-    }
-}
--- textmovie.js
+++ textmovie.js
@@ -1,3 +1,13 @@
+class Frame {
+    content;
+    classList;
+
+    constructor(content, classList) {
+        this.content = content;
+        this.classList = classList;
+    }
+}
+
 class Movie {
     movieElem;
     frameDuration;
